ed6dda6c455b95cd69e7de220fa2e4d6ffec3e7b
Eugene <korazon2271@gmail.com>
2022-06-12 22:21:20 +0300

    Исправление

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -10,8 +10,9 @@ public class StatisticsService {
   public long findMax(long[] incomes) {
     long currentMax = incomes[0];
     for (long income : incomes) {
-      if (currentMax < income || currentMax == income )
+      if (currentMax < income || currentMax == income) {
           currentMax = income;
+      }
     }
     return currentMax;
   }
